06887a28d1c3cf81044d01ecb27f47c96b5020a3
TribalHouse <36281410+TribalHouse@users.noreply.github.com>
2026-06-05 20:05:38 +0000

    Update PortBay cask to 0.1.3

--- Casks/portbay.rb
+++ Casks/portbay.rb
@@ -1,6 +1,6 @@
 cask "portbay" do
-  version "0.1.2"
-  sha256 "188991f5c9035b8b6773d249afa13b443784f078ea0c3017310dffbcf877d94f"
+  version "0.1.3"
+  sha256 "2e84f8a7e62dd23a9660d56dd70cc7dc9ec670f0454fb72f4fff547d247a2cbf"
 
   url "https://github.com/portbay-app/portbay/releases/download/v#{version}/PortBay_0.1.2_aarch64.dmg"
   name "PortBay"
